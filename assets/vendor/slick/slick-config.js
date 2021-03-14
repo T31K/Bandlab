b9116ef3b6b24667991545c43f3407667c20cc86
T31K <t31kmunwong@gmail.com>
2021-03-14 15:21:00 +0800

    Clean up code and removed unused JS vendor files

--- assets/vendor/slick/slick-config.js
+++ assets/vendor/slick/slick-config.js
@@ -1,5 +1,9 @@
 $('.slider-artist').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  arrows: false,
+  autoplay: true
+})
